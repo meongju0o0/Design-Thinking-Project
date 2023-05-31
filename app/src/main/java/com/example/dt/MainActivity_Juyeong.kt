1705b55c1bfd355b655089703455776d590b9ded
meongju0o0 <bl4angel20@khu.ac.kr>
2023-06-01 00:43:04 +0900

    change MainActivity_Juyeong

--- app/src/main/java/com/example/dt/MainActivity_Juyeong.kt
+++ app/src/main/java/com/example/dt/MainActivity_Juyeong.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity_Juyeong : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
