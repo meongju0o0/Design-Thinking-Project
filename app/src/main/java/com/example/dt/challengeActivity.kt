151cf10712642976aad48d4fb58d57c60d27b2e4
meongju0o0 <bl4angel20@khu.ac.kr>
2023-06-08 17:43:45 +0900

    챌린지 통합

--- app/src/main/java/com/example/dt/challengeActivity.kt
+++ app/src/main/java/com/example/dt/challengeActivity.kt
@@ -9,24 +9,28 @@ class challengeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_challenge)
-        // var declare
-        val speed: ImageButton = findViewById(R.id.button1)
+
+        val block: ImageButton = findViewById(R.id.button1)
         val message: ImageButton = findViewById(R.id.button2)
         val present: ImageButton = findViewById(R.id.button3)
         val donate: ImageButton = findViewById(R.id.button4)
 
-        speed.setOnClickListener {
-            val intent = Intent(this, OnchallengeActivity::class.java)
-            startActivity(intent)
+        val block_touch_intent = Intent(this, BlockTouchService::class.java)
+
+        block.setOnClickListener {
+            startActivity(block_touch_intent)
         }
+
         message.setOnClickListener {
             val intent = Intent(this, SetmessageActivity::class.java)
             startActivity(intent)
         }
+
         present.setOnClickListener {
             val intent = Intent(this, SetpresentActivity::class.java)
             startActivity(intent)
         }
+
         donate.setOnClickListener {
             val intent = Intent(this, SetdonateActivity::class.java)
             startActivity(intent)
