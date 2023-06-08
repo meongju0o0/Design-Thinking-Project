d676e87a5fcf852e94b32064b39ff1eca13810a3
meongju0o0 <bl4angel20@khu.ac.kr>
2023-06-08 17:32:54 +0900

    Integrated InitSetting

--- app/src/main/java/com/example/dt/InitsettingActivity.kt
+++ app/src/main/java/com/example/dt/InitsettingActivity.kt
@@ -1,8 +1,10 @@
 package com.example.dt
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
@@ -13,7 +15,7 @@ class InitsettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initsetting)
-        // var declare
+
         val timePicker1 = findViewById<TimePicker>(R.id.timePicker1)
         val start_hr = timePicker1.hour
         val start_min = timePicker1.minute
@@ -21,7 +23,6 @@ class InitsettingActivity : AppCompatActivity() {
         val end_hr = timePicker2.hour
         val end_min = timePicker2.minute
 
-        //var declare_chckbox
         val use_time: CheckBox = findViewById(R.id.check1) // 사용 시간
         val saying: CheckBox = findViewById(R.id.check2) // 명언 팝업
         val bluelight: CheckBox = findViewById(R.id.check3) // 블루라이트
@@ -32,6 +33,11 @@ class InitsettingActivity : AppCompatActivity() {
         var chk_asmr = false
 
         val apply: Button = findViewById(R.id.apply)
+
+        val use_time_intent = Intent(this, UseTimeService::class.java)
+        val saying_intent = Intent(this, SayingService::class.java)
+        val bluelight_intent = Intent(Settings.ACTION_DISPLAY_SETTINGS)
+        val asmr_intent = Intent(this, ASMRService::class.java)
 
         use_time.setOnCheckedChangeListener { buttonView, isChecked ->
             chk_use_time = isChecked
@@ -53,31 +59,37 @@ class InitsettingActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
             if (chk_use_time) {
-
+                // 휴대폰 사용 시간 notification
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(use_time_intent)
+                } else {
+                    startService(use_time_intent)
+                }
             }
             else {
-
+                stopService(use_time_intent)
             }
 
             if (chk_saying) {
-
+                startService(saying_intent)
             }
             else {
-
+                stopService(saying_intent)
             }
 
             if (chk_bluelight) {
-
-            }
-            else {
-
+                startActivity(bluelight_intent)
             }
 
             if (chk_asmr) {
-
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(asmr_intent)
+                } else {
+                    startService(asmr_intent)
+                }
             }
             else {
-
+                stopService(asmr_intent)
             }
         }
     }
